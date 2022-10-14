420ae2c9aa46fe48fb67a0c38f2f6087d23a3a90
sorriso <sorriso.tls@gmail.com>
2022-10-14 18:41:31 +0200

    #3

--- 1-start-jenkins.sh
+++ 1-start-jenkins.sh
@@ -1,8 +1,6 @@
 nerdctl -n k8s.io pull nginx:1.23.1-alpine
-nerdctl -n k8s.io pull jenkins/jenkins:2.361.1-lts-jdk11
-nerdctl -n k8s.io pull jenkins/jnlp-agent-docker:latest
-nerdctl -n k8s.io pull docker:latest
+nerdctl -n k8s.io pull jenkins/jenkins:2.361.2-lts-jdk11
 
-kubectl apply -f ./common/jenkins-namespace.yaml
+kubectl apply -f ./common/
 sleep 2
 kubectl apply -f jenkins
